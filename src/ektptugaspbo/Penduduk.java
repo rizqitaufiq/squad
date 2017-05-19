dac2598089b5769f3f5983c18fb98e5a6efff682
Publisher <Publisher@Publisher-PC>
2017-05-19 23:10:19 +0700

    Setter Getter (Taufik)

--- src/ektptugaspbo/Penduduk.java
+++ src/ektptugaspbo/Penduduk.java
@@ -11,7 +11,7 @@ package ektptugaspbo;
  */
 public class Penduduk {
      private String jeneng, agomo, kelamin, pengawean, getih, deso, RT, panggenlahir, tanggalLahir, negoroAsal;
-
+ 
     public Penduduk(String jeneng, String agomo, String kelamin, String pengawean, String getih, String deso, String RT, String panggenlahir, String tanggalLahir, String negoroAsal) {
         this.jeneng = jeneng;
         this.agomo = agomo;
