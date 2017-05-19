2038c225b182e81bbf23c527944681e85283ac72
Publisher <Publisher@Publisher-PC>
2017-05-19 23:08:30 +0700

    Squad one night clear

--- src/ektptugaspbo/Penduduk.java
+++ src/ektptugaspbo/Penduduk.java
@@ -12,10 +12,6 @@ package ektptugaspbo;
 public class Penduduk {
      private String jeneng, agomo, kelamin, pengawean, getih, deso, RT, panggenlahir, tanggalLahir, negoroAsal;
 
-    public Penduduk() {
-
-    }
-
     public Penduduk(String jeneng, String agomo, String kelamin, String pengawean, String getih, String deso, String RT, String panggenlahir, String tanggalLahir, String negoroAsal) {
         this.jeneng = jeneng;
         this.agomo = agomo;
